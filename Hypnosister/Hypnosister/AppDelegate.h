13f0b0054507395322c1b3ebd77c5701e0b9f76b
brentzucker <brentzucker@gmail.com>
2016-07-08 08:28:59 -0400

    created hypnosister project

---
+++ Hypnosister/Hypnosister/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hypnosister
+//
+//  Created by Brent Zucker on 7/8/16.
+//  Copyright © 2016 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hypnosister/Hypnosister/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hypnosister
+//
+//  Created by Brent Zucker on 7/8/16.
+//  Copyright © 2016 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
