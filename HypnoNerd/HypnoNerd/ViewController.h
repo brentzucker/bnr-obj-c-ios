97dc2883635f7fbe01d658f0d164ee01b60f6b58
Brent Zucker <brentzucker@Shukis-iMac.local>
2016-07-11 16:24:28 -0400

    created hypnoNerd Project

---
+++ HypnoNerd/HypnoNerd/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HypnoNerd
+//
+//  Created by Brent Zucker on 7/11/16.
+//  Copyright © 2016 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
